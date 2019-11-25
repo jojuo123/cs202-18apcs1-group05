0cd1e0687066b8f1e595e6c3b1444813b7cc692f
jojuo123 <vtduy3100@gmail.com>
2019-11-25 16:50:30 +0700

    Add files via upload

--- CrossingRoad/CrossingRoad/Level.h
+++ CrossingRoad/CrossingRoad/Level.h
@@ -16,17 +16,25 @@ class Level
 {
 private:
 	int noOfRoad;
-	int noOfLanePerRoad;
+	//int noOfLanePerRoad;
 	int LevelId;
-	int noOfDinoLane;
-	int noOfTigLane;
-	int noOfTruLane;
-	int noOfMotoLane;
+	//int noOfDinoLane;
+	//int noOfTigLane;
+	//int noOfTruLane;
+	//int noOfMotoLane;
 	static const int levelListRoad[5];
+	static const int levelListDinoLane[5];
+	static const int levelListTigLane[5];
+	static const int levelListTruckLane[5];
+	static const int levelListMotoLane[5];
+	static const int levelListGrassLane[5];
+	//2 row dau la grass
+	//row cuoi grass
+	
 public:
 	~Level() {}
 	Level() = default;
-	Level(int id);
+	Level(int id) : LevelId(id) {}
 	vector<ObjectType> getObstacle(); //random de tra ve list cac obs theo row
 	int FinishLane() { return noOfRoad; }
 	friend class Scene;
