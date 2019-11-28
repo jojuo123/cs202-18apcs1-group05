3e8ded3345b55e49d2e30c1f89b3f6afe0d65e82
gigajet <gigajet08092000@gmail.com>
2019-11-28 08:04:24 +0700

    Scene.h :: UpdateCamera()

--- CrossingRoad/CrossingRoad/Player.cpp
+++ CrossingRoad/CrossingRoad/Player.cpp
@@ -4,6 +4,32 @@ bool Player::isHit(const Obstacle* &obs) {
 	return position.intersects(obs->getPosition());
 }
 
+void Player::Move(Direction dir, objSize numPixel)
+{
+	switch (dir) {
+	case UP: {
+		coord.y++;
+		Object::Move(dir, numPixel);
+		break;
+	}
+	case DOWN: {
+		coord.y--;
+		Object::Move(dir, numPixel);
+		break;
+	}
+	case LEFT: {
+		coord.x--;
+		Object::Move(dir, numPixel);
+		break;
+	}
+	case RIGHT: {
+		coord.x++;
+		Object::Move(dir, numPixel);
+		break;
+	}
+	}
+}
+
 void Player::playSound()
 {
 	sf::Sound sound;
